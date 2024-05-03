21cf8b5b2255cca8c494c3c79194112920580770
Micha <michael.rigas@t-online.de>
2024-05-03 15:46:59 +0200

    Neues Repo erstellt und Änderungen integriert

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,22 @@
+// Zuerst werden die Kontanten festgestzt, die wir benötigen //
+const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+const AccessTokens = require("../services/auth/AccessToken");
+// Hier wird die entsprechende Middleware-Funktion geschrieben //
+function authMiddleWare(req, res, next) {
+  const token = req.headers["x-access-token"];
+  if (!token) {
+    return res.status(StatusCodes.FORBIDDEN).send("No Token provided!");
+  }
+  try {
+    const decodedAccessToken = AccessTokens.decodeAccessToken(token);
+    req.user = decodedAccessToken; // { userId: 1 }
+  } catch (e) {
+    return res
+      .status(StatusCodes.FORBIDDEN)
+      .send("Something is wrong with your token");
+  }
+
+  return next();
+}
+
+module.exports = authMiddleWare;
