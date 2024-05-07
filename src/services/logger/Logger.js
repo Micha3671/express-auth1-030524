ddf4757d36a9cdee4e5881a8bb0060afd99d2a49
Micha <michael.rigas@t-online.de>
2024-05-07 18:34:47 +0200

    Logger in .env sowie Verwendung

--- src/services/logger/Logger.js
+++ src/services/logger/Logger.js
@@ -1,23 +1,18 @@
 const winston = require("winston");
+require("dotenv").config(); // Importiere dotenv für das Lesen von Umgebungsvariablen
+
+const logLevel = process.env.LOG_LEVEL || "info"; // Standardmäßig "info", falls LOG_LEVEL nicht definiert ist
 
 const logger = winston.createLogger({
-  level: "info",
+  level: logLevel,
   format: winston.format.json(),
   defaultMeta: { service: "user-service" },
   transports: [
-    //
-    // - Write all logs with importance level of `error` or less to `error.log`
-    // - Write all logs with importance level of `info` or less to `combined.log`
-    //
     new winston.transports.File({ filename: "error.log", level: "error" }),
     new winston.transports.File({ filename: "combined.log" }),
   ],
 });
 
-//
-// If we're not in production then log to the `console` with the format:
-// `${info.level}: ${info.message} JSON.stringify({ ...rest }) `
-//
 if (process.env.NODE_ENV !== "production") {
   logger.add(
     new winston.transports.Console({
@@ -25,3 +20,5 @@ if (process.env.NODE_ENV !== "production") {
     })
   );
 }
+
+module.exports = logger;
